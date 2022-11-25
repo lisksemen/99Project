2acdb3c8fd26e0b73762b54726f1bfd0994de0d4
lisak <lisk.semen@gmail.com>
2022-11-25 16:59:29 +0100

    Initial commit

---
+++ src/main/java/com/inc/tarik/project99/database/Database.java
@@ -0,0 +1,7 @@
+package com.inc.tarik.project99.database;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Database {
+}
---
+++ src/main/java/com/inc/tarik/project99/dto/Faculty.java
@@ -0,0 +1,41 @@
+package com.inc.tarik.project99.dto;
+
+public enum Faculty {
+    IAT("ІАТ"),
+    IATE("IATE"),
+    IEE("ІЕЕ"),
+    IMZ("ІМЗ"),
+    IPSA("ІПСА"),
+    ITS("ІТС"),
+    MMI("ММІ"),
+    FTI("ФТІ"),
+    IHF("ІХФ"),
+    PBF("ПБФ"),
+    RTF("РТФ"),
+    FBMI("ФБМІ"),
+    FBT("ФБТ"),
+    FEA("ФЕА"),
+    FEL("ФЕЛ"),
+    FIOT("ФІОТ"),
+    FL("ФЛ"),
+    FMM("ФММ"),
+    FMF("ФМФ"),
+    FPM("ФПМ"),
+    FSP("ФСП"),
+    HTF("ХТФ"),
+    ISZZI("ІСЗЗІ");
+
+
+    private final String name;
+
+    Faculty(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
+
+}
